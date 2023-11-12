be2aa7867ba553c36edadb1a474981d6223da265
Kupda <kupda27@gmail.com>
2023-11-12 20:16:35 +0300

    Almost complete data cleanup

--- main.py
+++ main.py
@@ -2,49 +2,24 @@
 import sys
 import pprint
 import re
+import unicodedata
 
 # Чтение какого-то файла
-with open('docs/utf8/128', 'r', -1, 'UTF-8') as file:
-    lines = file.readlines()
+with open('docs/utf8/143', 'r', -1, 'UTF-8') as file:
+    lines = file.read()
 
 
 def clearing_data(text):  # Очистка данных
-    """new_text = text.replace(',', '')
-    new_text = new_text.replace(':', '')
-    new_text = new_text.replace('…', '.')
-    new_text = new_text.replace('?', '.')
-    new_text = new_text.replace('!', '.')
-    new_text = new_text.replace('-', '')
-    new_text = new_text.replace('  ', ' ')
-    new_text = new_text.replace('“', '')
-    new_text = new_text.replace("”", '')
-    new_text = new_text.replace("\n", ' ')
-    new_text = new_text.replace("(", '')
-    new_text = new_text.replace(")", '')
-    new_text = new_text.replace("'", '')
-    new_text = new_text.replace('"', '')
-    new_text = new_text.replace('—', '')
-    new_text = new_text.replace('/', '')
-    #new_text = re.sub("[,|:|“|”|(|)|'|-|/|—]", "", text)
-    #new_text = re.sub("[?|!|...|…|]", ".", new_text)
-    #new_text = re.sub("[\n|  |\xa0|]", " ", new_text)
-    # В итоге мы в тексте оставляем все буквы, точки и пробелы (исправьте меня, если я что-то забыл)
-    new_text = re.sub("[^A-Za-zА-Яа-я.\n ]", '', text)
+    # Убираем неразрывный пробел
+    new_text = unicodedata.normalize("NFKD", text)
+
+    # В тексте оставляем все буквы, цифры, точки и пробелы (исправьте меня, если я что-то забыл)
+    new_text = re.sub("[^A-Za-zА-Яа-я.0-9-\n ]", '', new_text)
+
+    # Теряются несколько точек
     new_text = new_text.replace('\n', ' ')
-    new_text = new_text.replace(' . ', '')
-    new_text = new_text.replace('  ', ' ')
     return new_text
-    # В итоге мы в тексте оставляем все буквы, цифры, точки и пробелы (исправьте меня, если я что-то забыл)
-    new_text = []
-    for i in text:
-        if i != '\n':
-            fragment = re.sub("[^A-Za-zА-Яа-я.0-9-\n ]", '', i)
-            if fragment != '':
-                new_text.append(fragment)
-
-    return new_text
-    """
 
 
-# print(clearing_data(lines))
 pprint.pprint(clearing_data(lines))
+
