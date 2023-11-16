14a5eef6030dffd143d84cb4ea464ae63d50a661
Kupda <kupda27@gmail.com>
2023-11-16 22:53:12 +0300

    big update (помогите с очисткой данных)

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # рис. что делать??
 
 # Импортирование библиотек
-import sys
 import pprint
 import re
 import unicodedata
@@ -10,23 +9,18 @@ from pymorphy3 import MorphAnalyzer
 
 
 morph = MorphAnalyzer()
-doc = 143  # файл
-# Чтение какого-то файла
+doc = 143
 with open(f'docs/utf8/{doc}', 'r', -1, 'UTF-8') as file:
     lines = file.read()
 
 
 def clearing_data(text):  # Очистка данных
-    # Убираем неразрывный пробел
     new_text = unicodedata.normalize("NFKD", text)
     new_text = new_text.replace('-', ' ')
     new_text = new_text.replace('.', ' ')
     new_text = new_text.replace('@', ' ')
     new_text = new_text.replace('_', ' ')
-    # В тексте оставляем все буквы, цифры, точки и пробелы (исправьте меня, если я что-то забыл)
     new_text = re.sub("[^A-Za-zА-Яа-я\n ]", '', new_text)
-
-    # Теряются несколько точек
     new_text = new_text.replace('\n', ' ')
     return new_text
 
@@ -42,8 +36,6 @@ def tokenization(text):  # Токенизация
 def deleting_stop_words(text):  # Удаление стоп-слов
     new_text = []
     stop_words = get_stop_words('russian')
-    #stop_words1 = get_stop_words('english')
-    #stop_words = stop_words1 + stop_words
     for i in text:
         if not (i in stop_words):
             new_text.append(i)
@@ -71,8 +63,6 @@ def word_counter(text):  # Счетчик слов
             words_dict[word] = 1
     return sorted(words_dict.items(), key=lambda item: item[1], reverse=True)
 
-    # sorted_people = sorted(people.items(), key=lambda item: item[1])
-
 
 def vectorization(text):  # Векторизация
     pass
@@ -81,11 +71,8 @@ def vectorization(text):  # Векторизация
 text = clearing_data(lines)
 text = changing_case(text)
 text = tokenization(text)
-
 text = lemmatization(text)
-
 text = deleting_stop_words(text)
-#text = lemmatization(text)
 text = word_counter(text)
 pprint.pprint(text)
 # print(deleting_stop_words(text))
