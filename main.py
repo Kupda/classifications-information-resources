b39b3e62b23d1d14073ca55fc444b654a07bf374
Kupda <kupda27@gmail.com>
2023-11-16 20:53:57 +0300

    big update (помогите с очисткой данных)

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import sys
 import pprint
 import re
 import unicodedata
+from stop_words import get_stop_words
+from pymorphy3 import MorphAnalyzer
 
 
+morph = MorphAnalyzer()
 doc = 143  # файл
 # Чтение какого-то файла
 with open(f'docs/utf8/{doc}', 'r', -1, 'UTF-8') as file:
@@ -16,7 +19,7 @@ def clearing_data(text):  # Очистка данных
     new_text = unicodedata.normalize("NFKD", text)
 
     # В тексте оставляем все буквы, цифры, точки и пробелы (исправьте меня, если я что-то забыл)
-    new_text = re.sub("[^A-Za-zА-Яа-я.0-9-\n!? ]", '', new_text)
+    new_text = re.sub("[^A-Za-zА-Яа-я-\n ]", '', new_text)
 
     # Теряются несколько точек
     new_text = new_text.replace('\n', ' ')
@@ -32,11 +35,32 @@ def tokenization(text):  # Токенизация
 
 
 def deleting_stop_words(text):  # Удаление стоп-слов
-    pass
+    new_text = []
+    stop_words = get_stop_words('russian')
+    stop_words1 = get_stop_words('english')
+    stop_words = stop_words1 + stop_words
+    for i in text.split():
+        if not (i in stop_words):
+            new_text.append(i)
+    return new_text
 
 
 def lemmatization(text):  # Лемматизация
-    pass
+    tokens = []
+    for i in text:
+        tokens.append(morph.parse(i)[0].normal_form)
+    return tokens
+
+
+def word_counter(text):  # Счетчик слов
+    words_dict = {}
+
+    for word in text:
+        if word in words_dict:
+            words_dict[word] = words_dict[word] + 1
+        else:
+            words_dict[word] = 1
+    return words_dict
 
 
 def vectorization(text):  # Векторизация
@@ -45,5 +69,8 @@ def vectorization(text):  # Векторизация
 
 text = clearing_data(lines)
 text = changing_case(text)
+text = deleting_stop_words(text)
+text = lemmatization(text)
+text = word_counter(text)
 pprint.pprint(text)
-print()
+# print(deleting_stop_words(text))
