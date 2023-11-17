2092d4012dac23dfd15631db645566409137b5f4
Kupda <kupda27@gmail.com>
2023-11-17 18:24:41 +0300

    something

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
 # рис. - 143
 # свой - 100
-#
+# 102 файл надо посмотреть
+# И очистку данных
 
 # Импортирование библиотек
 import pprint
 import re
 import unicodedata
+from nltk.corpus import stopwords
 from stop_words import get_stop_words
 from pymorphy3 import MorphAnalyzer
+import nltk
+from nltk.tokenize import sent_tokenize, word_tokenize
 
+nltk.download('punkt')
 
 morph = MorphAnalyzer()
-doc = 100
+doc = 102
 with open(f'docs/utf8/{doc}', 'r', -1, 'UTF-8') as file:
     lines = file.read()
 
 
 def clearing_data(text):  # Очистка данных
     new_text = unicodedata.normalize("NFKD", text)
-    new_text = new_text.replace('-', ' ')
-    new_text = new_text.replace('.', ' ')
-    new_text = new_text.replace('@', ' ')
-    new_text = new_text.replace('_', ' ')
-    new_text = re.sub("[^A-Za-zА-Яа-я\n ]", '', new_text)
-    new_text = new_text.replace('\n', ' ')
+    # new_text = new_text.replace('-', ' ')
+    # new_text = new_text.replace('.', ' ')
+    # new_text = new_text.replace('@', ' ')
+    # new_text = new_text.replace('_', ' ')
+    new_text = re.sub("[^A-Za-zА-Яа-я]", ' ', new_text)
+    #new_text = new_text.replace('\n', ' ')
     return new_text
 
 
@@ -32,7 +37,8 @@ def changing_case(text):  # Изменение регистра
 
 
 def tokenization(text):  # Токенизация
-    return text.split()
+    #print(text)
+    return word_tokenize(text, language="russian")
 
 
 def deleting_stop_words(text):  # Удаление стоп-слов
@@ -42,6 +48,10 @@ def deleting_stop_words(text):  # Удаление стоп-слов
         if not (i in stop_words) and len(i) != 1:
             new_text.append(i)
     return new_text
+    # lemmatize = nltk.WordNetLemmatizer()
+    # lemmatize.lemmatize(text)
+    # lemmatize = (for word in text if not text in set(stopwords.words("stopwords")))
+    # return lemmatize
 
 
 def lemmatization(text):  # Лемматизация
@@ -49,15 +59,15 @@ def lemmatization(text):  # Лемматизация
     for i in text:
         p = morph.parse(i)[0]
         if p.tag.POS != 'VERB' and p.tag.POS != 'INFN':
-            #if i == 'года':
-                tokens.append(morph.parse(i)[0].normal_form)
-                #print(morph.parse(i)[0].normal_form)
+            tokens.append(morph.parse(i)[0].normal_form)
     return tokens
+    #lemmatize = nltk.WordNetLemmatizer()
+    #text = [lemmatize.lemmatize(word) for word in i]
+    #return text
 
 
 def word_counter(text):  # Счетчик слов
     words_dict = {}
-
     for word in text:
         if word in words_dict:
             words_dict[word] = words_dict[word] + 1
